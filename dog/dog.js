5ed7af82ce49fec1f85e5933e7d260ed20781aa3
Lusine Sargsyan <sargsyanlq@gmail.com>
2021-04-10 20:29:42 +0400

    separate div from button for dogs

--- dog/dog.js
+++ dog/dog.js
@@ -1,10 +1,42 @@
 const favoriteDogList = document.querySelector('#favorite-dogs')
 console.log(favoriteDogList);
 const searchButton = document.querySelector('.search-dog-image');
+const forRender = document.querySelector('.render-image');
 
 //https://dog.ceo/api/breeds/list/all
 //https://dog.ceo/api/breed/hound/images
 
+searchButton.addEventListener('click', (event) => {
+  //console.log(event.target.value, 'serach');
+  console.log(favoriteDogList.value, 'value::');
+  const searchValue = favoriteDogList.value;
+
+
+  fetch(`https://dog.ceo/api/breed/${searchValue}/images`)
+  .then((resopnse) => {
+    console.log(resopnse, 'search dogs response');
+    return resopnse.json();
+  })
+  .then(({message}) => {
+    console.log(message, 'all images link of dogs');
+
+    const div = document.createElement('div');
+    const img = document.createElement('img');
+    const img2 = document.createElement('img');
+    img.setAttribute('src', message[0]);
+    img2.setAttribute('src', message[1]);
+    img.style.width = '250px';
+    img2.style.width = '250px';
+    div.append(img);
+    div.append(img2);
+    forRender.append(div);
+  });
+
+  
+});
+
+
+// the second way for all dogs images
 searchButton.addEventListener('click', (event) => {
   //console.log(event.target.value, 'serach');
   console.log(favoriteDogList.value, 'value::');
@@ -28,13 +60,12 @@ searchButton.addEventListener('click', (event) => {
       //img.classList.add('dog-image');
       img.style.width = '250px';
       div.append(img);
-      searchButton.append(div);
+      forRender.append(div);
     });
 
   });
 
   
 });
-
 
 
--- film/film.js
+++ film/film.js
@@ -5,95 +5,42 @@ const filmContainer = document.querySelector('.film-data');
 const BASE_URL = 'https://ghibliapi.herokuapp.com/films';
 
 
-// the same as searchInput.value = '';
-const searcհState = {
-  value: ''
-}
+searchButton.addEventListener('click', (event) => {
 
-const getFilms = () => {
-  fetch(`${BASE_URL}${searcհState.value}`)
+  fetch(`${BASE_URL}`)
   .then((response) => {
     console.log(response);
     //console.log(response.json());
     return response.json();
   })
-  .then((films) => {
-    console.log(films);
+  .then((film) => {
+    console.log(film);
+    film.forEach((everyFilm, index) => {
+      //console.log(everyFilm.producer, everyFilm.title, everyFilm.description,  '::::');
+      const div = document.createElement('div');
+      div.classList.add('film-container');
 
-    
+      const makeElement = () => {
+        const dataBlocks = ['Title', 'Description', 'Director', 'Producer', 'Relase date'];
 
-    //return render(film);
-  })
-}
+        return dataBlocks.map((elem) => {
+          //console.log(elem);
 
-searchButton.addEventListener('click', () => {
-  searcհState.value = searchInput.value;
-  getFilms();
-})
+          const divBlock =  document.createElement('div');
+          divBlock.classList.add('film-blok');
+          divBlock.textContent = `${elem}`;
+          const title = document.createElement('span');
+          title.textContent = `${everyFilm[elem.toLowerCase()] || everyFilm['relase_date']}`;//everyFilm.relase_date
+          divBlock.append(title);
+          title.classList.add('block-title');
 
-const render = (films) => {
-  films.forEach((film) => {
-    const div = document.createElement('div');
-    div.classList.add('film-container');
-    const makeElement = () => {
-      const dataBlocks = ['Title', 'Description', 'Director', 'Producer', 'Relase date'];
-
-      return dataBlocks.map((elem) => {
-        //console.log(elem);
-
-        const divBlock =  document.createElement('div');
-        divBlock.classList.add('film-blok');
-        divBlock.textContent = `${elem}`;
-        const title = document.createElement('span');
-        title.textContent = `${film[elem.toLowerCase()] || film['relase_date']}`;//everyFilm.relase_date
-        divBlock.append(title);
-        title.classList.add('block-title');
-
-        return divBlock;
-      });
-    };
-
-    div.append(...makeElement());
-    filmContainer.append(div);
+          return divBlock;
+        });
+      };
+      div.append(...makeElement());
+      filmContainer.append(div);
+      searchInput.innerHTML = '';
+    });
   });
-};
-
-// searchButton.addEventListener('click', (event) => {
-
-//   fetch(`${BASE_URL}`)
-//   .then((response) => {
-//     console.log(response);
-//     //console.log(response.json());
-//     return response.json();
-//   })
-//   .then((film) => {
-//     console.log(film);
-//     film.forEach((everyFilm, index) => {
-//       //console.log(everyFilm.producer, everyFilm.title, everyFilm.description,  '::::');
-//       const div = document.createElement('div');
-//       div.classList.add('film-container');
-
-//       const makeElement = () => {
-//         const dataBlocks = ['Title', 'Description', 'Director', 'Producer', 'Relase date'];
-
-//         return dataBlocks.map((elem) => {
-//           //console.log(elem);
-
-//           const divBlock =  document.createElement('div');
-//           divBlock.classList.add('film-blok');
-//           divBlock.textContent = `${elem}`;
-//           const title = document.createElement('span');
-//           title.textContent = `${everyFilm[elem.toLowerCase()] || everyFilm['relase_date']}`;//everyFilm.relase_date
-//           divBlock.append(title);
-//           title.classList.add('block-title');
-
-//           return divBlock;
-//         });
-//       };
-//       div.append(...makeElement());
-//       filmContainer.append(div);
-//       searchInput.innerHTML = '';
-//     });
-//   });
   
-// });  
+});  
